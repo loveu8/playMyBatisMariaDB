65cced73970cfd534c70b9c9f7f66bad692e8ee6
LornGhu <lornghu@eztravel.com.tw>
2016-08-18 17:36:58 +0800

    新增第八章範例

--- app/controllers/ApiController.java
+++ app/controllers/ApiController.java
@@ -1,8 +1,20 @@
 package controllers;
 
+import play.libs.Json;
 import play.mvc.Controller;
+import play.mvc.Result;
+import pojo.game.Player;
 
 public class ApiController extends Controller{
+	
+	// 取得遊戲管理者角色資料
+	public Result getGameMasterData(){
+		Player player = new Player();
+		player.setName("GM");
+		player.setSex("Male");
+		player.setRace("human");
+		return ok (Json.toJson(player));
+	}
 	
 	
 }
---
+++ app/pojo/game/Player.java
@@ -0,0 +1,38 @@
+package pojo.game;
+
+public class Player {
+	
+	// 遊戲角色姓名
+	private String name;
+	
+	// 性別
+	private String sex;
+	
+	// 種族
+	private String race;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public String getRace() {
+		return race;
+	}
+
+	public void setRace(String race) {
+		this.race = race;
+	}
+
+}
