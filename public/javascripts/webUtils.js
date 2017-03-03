512f6bae794aff35578223439e990e4c02c97268
LornGhu <lornghu@eztravel.com.tw>
2017-03-03 17:28:01 +0800

    調整ajax

--- public/javascripts/webUtils.js
+++ public/javascripts/webUtils.js
@@ -1,49 +1,37 @@
+//讀取圖片資料與驗證
+function checkImage (url){
+    var s = document.createElement("IMG");
+    s.src = url;
+    return s;
+}
 
 // 傳入Input欄位ID與要顯示imgID，檢查是否要顯示圖片
-function imgBlurHandler(inputUrl , imgUrl){
-	$(inputUrl).blur("change paste keyup", function() {
-		if(!isURL($(inputUrl).val())){
-	  		console.log("this url is not right , url = " + $(inputUrl).val());
-    		$(imgUrl).hide();
+function imgBlurHandler(inputUrlId , imgUrlId){
+	$(inputUrlId).blur("change paste keyup", function() {
+		var url = $(inputUrlId).val();
+		if(!isURL($(inputUrlId).val())){
+	  		console.log("this url is not right , url = " + url);
+	  		$(imgUrlId).hide();
 	  		return;
 		}
-		var checkImage = function(url){
-		    var s = document.createElement("IMG");
-		    s.src = url;
-		    s.onerror = function(){
-		  		console.log("this image url is invalid , url = " + url);
-	    		$(imgUrl).hide();
-		    }
-		    s.onload = function(){
-		        console.log("this image url is valid , url = " + url);
-		    	$(imgUrl).attr("src" , $(inputUrl).val());
-		    	$(imgUrl).show();
-		    }
-		}
-      	checkImage($(inputUrl).val()); 
+	   	var img = checkImage(url); 
+	   	img.onerror = function(){
+			console.log("this image url is invalid , url = " + url);
+			$(imgUrlId).hide();
+			return;
+	    }
+	    img.onload = function(){
+	        console.log("this image url is valid , url = " + url);
+	      	$(imgUrlId).attr("src" , $(inputUrlId).val());
+	      	$(imgUrlId).show();
+	    }
 	});
 }	
-
-// 確認是否是網址
+  
+  // 確認是否是網址
 function isURL(str) {
-  var pattern = new RegExp(/^(http|https|ftp):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/i); 
-  return pattern.test(str);
+    var pattern = new RegExp(/^(http|https|ftp):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/i); 
+    return pattern.test(str);
 }
 
 
-// 傳入要驗證的使用者姓名
-function userNameBlurHandler(inputName , checkMessage, ajaxUrl){
-	$(inputName).blur("change paste keyup", function() {
-		var checkUrl = ajaxUrl + $("#username").val();
-		console.log("checkUrl = " + checkUrl);
-		$.getJSON( checkUrl, function( data ) {
-			$(checkMessage).html('');
-		$(checkMessage).append(data.statusDesc);
-			if(data.status!=200){
-				$(checkMessage).css("color", "red"); 
-  	  		} else {
-				$(checkMessage).css("color", "green"); 
-  	  	  	}
-		});
-	});
-}
