25470b155409a9861ec8390c223a886527b2e281
LornGhu <lornghu@eztravel.com.tw>
2017-10-21 11:03:30 +0800

    回文按鈕js設計

--- public/javascripts/article.js
+++ public/javascripts/article.js
@@ -1,19 +1,29 @@
+// 回覆表單
 function replyFnc(){
+	// 發現有回覆表單時，不再產生
+    if($(this).next('form')!=null && $(this).next('form').length > 0){
+    	console.log('same form not create new!!');
+    	return;
+    }
 	var inputText = 
 		'<form action="" id="confirmationForm" method="post">'
 		+'<textarea id="confirmationText" class="text" cols="30" rows ="5" name="confirmationText" form="confirmationForm"></textarea>'
 		+'<button type="submit" class = "submitButton">回覆</button>'
 		+'<button type="button" onclick = "restoreComments()" class="submitButton">取消</button>'
 		+'</form>';
-	$(this).append(inputText);
-	$(this).unbind("click");
+	// 回覆按鈕隱藏
+	$(this).hide();
+	// 新增reply form
+	$(this).after(inputText);
+	console.log('reply form create ok!!');
 }
 
+// 移除表單
 function restoreComments(){
-	   $("#confirmationForm").remove();
-	   $(".icon.fa-comments").each(function() {
-		   $(this).bind( 'click', replyFnc);
-	   });
+	// 恢復回覆按鈕
+	$('#confirmationForm').prev().show();
+	// 移除回覆表單
+	$('#confirmationForm').remove();
 }
 
 $(document).ready(function() {
@@ -48,7 +58,7 @@ $(document).ready(function() {
         return false;
     });
     
-    
+    // 設定回覆按鈕事件
     $(".icon.fa-comments").click(replyFnc);
         
 });
